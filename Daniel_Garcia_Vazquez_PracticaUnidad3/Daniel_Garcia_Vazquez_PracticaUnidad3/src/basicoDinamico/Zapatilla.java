481b29b0eed554fc2df034d68c8b20bbe3cacdaa
Dani García <daninoventayocho@gmail.com>
2022-11-18 20:02:49 +0100

    Practica Unidad 3

---
+++ Daniel_Garcia_Vazquez_PracticaUnidad3/Daniel_Garcia_Vazquez_PracticaUnidad3/src/basicoDinamico/Zapatilla.java
@@ -0,0 +1,55 @@
+package basicoDinamico;
+
+
+import javafx.beans.property.SimpleStringProperty;
+
+/**
+ * 
+ * Clase auxiliar que se empleará para el modelo de datos del TableView
+ * 
+ * @author SBF
+ * 
+ */
+public class Zapatilla {
+    private final SimpleStringProperty Name;
+    private final SimpleStringProperty Stock;
+   
+    
+ 
+    /**
+	 * 
+	 */
+	public Zapatilla() {
+		this(null,null);
+	}
+
+	public Zapatilla(String Name,String Stock) {
+        this.Name = new SimpleStringProperty(Name);
+        this.Stock = new SimpleStringProperty(Stock);
+       
+    }
+ 
+    public String getName() {
+        return Name.get();
+    }
+    
+    public void setName(String fName) {
+    	Name.set(fName);
+    }
+    public SimpleStringProperty getNameProperty() {
+        return Name;
+    }
+        
+    public String getStock() {
+        return Stock.get();
+    }
+    public void setStock (String fName) {
+    	Stock.set(fName);
+    }
+    public SimpleStringProperty getStockProperty() {
+        return Stock;
+    }
+    
+    
+        
+}
